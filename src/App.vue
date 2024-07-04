3a23e89233b3372e0387a206ce66c891e251074e
Michel Souza <Michelnsouza98@gmail.com>
2024-07-03 21:10:36 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+  </header>
+  <main>
+    <RouterView />
+  </main>
+  <footer>
+
+  </footer>
+
+</template>
+
+<style scoped>
+
+</style>
