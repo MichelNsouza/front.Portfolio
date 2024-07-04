10372d12b5b2b6bbba28edbdb5aece62312445fe
Michel Souza <Michelnsouza98@gmail.com>
2024-07-04 18:10:49 -0300

    implementação loading

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
-<script setup>
+<script>
 import {RouterView } from 'vue-router';
-
 import FooterComponente from '@/components/FooterComponente.vue';
 import HeaderComponente from '@/components/HeaderComponente.vue';
+import LoadingComponente from '@/components/LoadingComponente.vue';
+
+export default {
+  components: {
+    HeaderComponente,
+    FooterComponente,
+    LoadingComponente
+  }
+}
 
 </script>
 
 <template>
 
+  <LoadingComponente />
+  
   <HeaderComponente />
 
   <main class="content bg-primary-subtle pt-3">
@@ -25,5 +35,8 @@ html {
 .content {
   width: 100%;
   min-height: calc(100vh - 100px);
+}
+.modal {
+  z-index: 1050; /* Z-index alto para garantir que o modal esteja sobre todo o conteúdo */
 }
 </style>
---
+++ src/components/LoadingComponente.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="isLoading" class="modal d-block position-fixed top-0 start-0 end-0 bottom-0 bg-primary-subtle" tabindex="-1">
+    
+    <div class="d-flex justify-content-center align-items-center h-100">
+
+      <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">A pagina esta carregando...</span>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1500);
+  },
+};
+</script>
+
+<style scoped>
+</style>
