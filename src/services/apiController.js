47233444be69ca0812941b7242d691a40e568dbd
Michel Souza <91084191+MichelNsouza@users.noreply.github.com>
2024-07-03 23:17:36 -0300

    correção link api

--- src/services/apiController.js
+++ src/services/apiController.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// const api = axios.create({
+//   baseURL: 'http://127.0.0.1:8000/api/projetos/api/'
+// });
+
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/projetos/api/api'
+  baseURL: 'https://api-portfolio-michel-souza.vercel.app/api/api'
 });
 
 export const todosProjetos = () => api.get('/projetos/');
