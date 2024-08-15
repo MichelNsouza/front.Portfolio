210b77177bb7311839e477953cbfdf8ac7077c3c
Michel Souza <91084191+MichelNsouza@users.noreply.github.com>
2024-08-15 10:07:37 -0300

    teste responsividade

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -57,11 +57,11 @@ export default {
           <div class="bg-conteudo card d-flex align-items-center justify-content-center">
             <div class="card-body">
               <p class="text-center mb-3 fs-1">Contato</p>
-              <div class="d-flex flex-lg-row flex-sm-column gap-2">
-                <a class="no-underline-link m-lg-3" href="https://www.linkedin.com/in/michel-n-souza/" target="_blank"><i class="bi bi-linkedin"></i> Vamos nos conectar?</a>
-                <a class="no-underline-link m-lg-3" href="https://github.com/MichelNsouza" target="_blank"><i class="bi bi-github"></i> GitHub</a>
-                <a class="no-underline-link m-lg-3" href="mailto:michelnsouza98@gmail.com?Subject=Olá Michel, vamos conversar! ?Body=Me manda uma mensagem :)" target="_blank"><i class="bi bi-envelope"></i> Michelnsouza98@gmail.com</a>
-                <a class="no-underline-link m-lg-3" href="https://wa.me/5571993125797/?text=Oi+Michel%2C+vim+atrav%C3%A9s+de+seu+portf%C3%B3lio%2C+vamos+conversar%3F%21" target="_blank"><i class="bi bi-whatsapp"></i> WhatsApp</a>
+              <div class="d-flex flex-column flex-md-row gap-2">
+                <a class="no-underline-link m-2" href="https://www.linkedin.com/in/michel-n-souza/" target="_blank"><i class="bi bi-linkedin"></i> Vamos nos conectar?</a>
+                <a class="no-underline-link m-2" href="https://github.com/MichelNsouza" target="_blank"><i class="bi bi-github"></i> GitHub</a>
+                <a class="no-underline-link m-2" href="mailto:michelnsouza98@gmail.com?Subject=Olá Michel, vamos conversar! ?Body=Me manda uma mensagem :)" target="_blank"><i class="bi bi-envelope"></i> Michelnsouza98@gmail.com</a>
+                <a class="no-underline-link m-2" href="https://wa.me/5571993125797/?text=Oi+Michel%2C+vim+atrav%C3%A9s+de+seu+portf%C3%B3lio%2C+vamos+conversar%3F%21" target="_blank"><i class="bi bi-whatsapp"></i> WhatsApp</a>
               </div>
             </div>
           </div>
